a9d1c107a5213c06616797de1c270fb63714cb4a
mike <mikelevitskiy@gmail.com>
2024-01-16 12:04:50 -0600

    Value Objects with abstract base class.

--- Marketplace/Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace/Marketplace.Domain/ClassifiedAd.cs
@@ -4,24 +4,19 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAd
     {
-        public Guid Id { get; }
+        public ClassifiedAd Id { get; }
 
-        public ClassifiedAd(Guid id)
+        public ClassifiedAd(ClassifiedAd id, UserId ownerId)
         {
-            if (id == default)
-            {
-                throw new ArgumentException(
-                    "Identity must be specified", nameof(id));
-            }
-
             Id = id;
+            _ownerId = ownerId;
         }
 
         public void SetTitle(string title) => _title = title;
         public void UpdateText(string text) => _text = text;
         public void UpdatePrice(decimal price) => _price = price;
 
-        private Guid _ownerId;
+        private UserId _ownerId;
         private string _title;
         private string _text;
         private decimal _price;
---
+++ Marketplace/Marketplace.Domain/Price.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Marketplace.Domain
+{
+    public class Price : Money
+    {
+        public Price(decimal amount) : base(amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Price cannot be negative", nameof(amount));
+            }
+        }
+    }
+}
